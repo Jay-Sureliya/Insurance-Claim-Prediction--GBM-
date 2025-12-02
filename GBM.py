534cd6b4a825de059638b15b63025cb36fcd0378
jay <jsureliya732@rku.ac.in>
2025-12-02 11:15:09 +0530

    FINAL

--- GBM.py
+++ GBM.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score , confusion_matrix , classification_report
 
@@ -24,6 +26,19 @@ model.fit(X_train,y_train)
 
 m_pred = model.predict(X_test)
 
-print("Accuracy is :- ",round(accuracy_score(y_test,m_pred)*100,2),"%")
+
 print("Confusion Matrix :- \n",confusion_matrix(y_test,m_pred))
-print("Classification Report :- \n",classification_report(y_test,m_pred))
+
+
+param_grid = {
+    'n_estimators': [50, 100, 150],
+    'learning_rate': [0.01, 0.1, 1.0],
+    'max_depth': [1, 3, 5]
+}
+
+grid_search = GridSearchCV(estimator=GradientBoostingClassifier(), param_grid=param_grid, cv=5)
+grid_search.fit(X_train, y_train)
+best_model = grid_search.best_estimator_
+best_pred = best_model.predict(X_test)
+best_score = cross_val_score(best_model, X_train, y_train, cv=5)
+print("Tuned Model Accuracy :- ", round(np.mean(best_score)*100,2))
